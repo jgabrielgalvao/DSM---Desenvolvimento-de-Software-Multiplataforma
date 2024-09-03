5af5fa2ddc4592233ba043ea7ea0f8834422b6e3
jgabrielgalvao <josegabriel755@gmail.com>
2024-09-03 20:11:40 -0300

    EA

---
+++ 4º semestre/estatistica-2-semestre/atividades/atividade 03-09/09)-EA.py
@@ -0,0 +1,6 @@
+# DESVIO PADRÃO
+
+# Calcula o afastamento dos dados em torno da média;
+# Este indicador complementa à média, quanto maior pior é, ou seja, isso
+# # significa o grau de heterogeneidade dos dados;
+
